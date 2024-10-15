c27731ea168957e9e48ffce6a0695ebc22967209
Tomas Glabazna <tomas@glabazna.cz>
2024-10-15 18:30:06 +0200

    cmt2

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,37 @@
 <template>
+  <TNav :links="links"/>
   <RouterView />
+  <button @click="onClick('/')">home</button>
+  <button @click="onClick('/about')">about me</button>
+  <button @click="onClick('/contact')">contact</button>
 </template>
+
+<script>
+import TNav from '@/components/TNav.vue'
+export default {
+  name: 'App',
+  data () {
+    return {
+      links: [
+        { path: '/', label: 'home' },
+        { path: '/about', label: 'about me' },
+        { path: '/contact', label: 'contact' }
+      ]
+    }
+  },
+  methods: {
+    onClick (path) {
+      this.$router.push(path)
+      /*
+        this.$router.push (path) - přesměruje na path
+        this.$router.back() - o jednu zpět v historii
+        this.$router.forward() - o jednu vpřed v historii
+        this.$router.go(n) - o n vpřed v historii
+        this.$router.go(-n) - o n vzad v historii
+      */
+    }
+  },
+  components: { TNav }
+}
+
+</script>
---
+++ src/components/TNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="link in links">
+        <RouterLink v-if="$route.path !== link.path" :to="link.path">{{ link.label }}</RouterLink>
+        <div v-else>{{ link.label }}</div>
+      </li>
+    </ul>
+    <!-- <div>{{ $route.path }}</div> -->
+  </nav>
+</template>
+
+<script>
+
+// this.$route - informace o dané route
+// this.$router - objekt obsahující routovací metody
+
+export default {
+  name: 'TNav',
+  props: {
+    links: Array
+  }
+}
+
+</script>
+<style scoped>
+  nav {
+    background: transparent;
+  }
+  ul {
+    list-style-type: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+    gap: 1rem;
+  }
+  a, div {
+    display: block;
+    padding: .4em .8em;
+  }
+</style>
